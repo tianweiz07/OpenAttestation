b104c8510460bb1866120268071bb9ea2258d3f3
tianweiz07 <tianweiz@princeton.edu>
2014-09-10 12:43:57 -0400

    tianweiz: attestation_kernel on the client side

--- Source/HisClient/src/gov/niarl/his/attestation_kernel.c
+++ Source/HisClient/src/gov/niarl/his/attestation_kernel.c
@@ -5,19 +5,18 @@
 #include <openssl/md5.h>
 
 #define FILE_LOCATION "/root/tpm-emulator/PCR_VALUE"
+#define IMAGE_LOCATION "/opt/stack/data/nova/instances/"
 
-// Compile: gcc -o attestation_kernel attestation_kernel.c -lcrypto -lssl
+// Compile: ./attestation_kernel vid secproperty -lcrypto -lssl
 
 int main(int argc, char *argv[])
 {
     int pcr_index = atoi(argv[2]);
-    printf("index is: %d\n", pcr_index);
 
     char file_address[256];
-    strcpy(file_address, "/opt/stack/data/nova/instances/");
+    strcpy(file_address, IMAGE_LOCATION);
     strcat(file_address, argv[1]);
     strcat(file_address, "/kernel");
-    printf("file address: %s\n", file_address);
 
     FILE *image_file = fopen(file_address, "rb");
     assert(image_file);
@@ -26,7 +25,7 @@ int main(int argc, char *argv[])
     int bytes;
     char image_value[1024];
     unsigned char hash_value[MD5_DIGEST_LENGTH];
-    char hash_value1[MD5_DIGEST_LENGTH];
+    char per_value;
 
     MD5_Init(&mdContext);
     while ((bytes = fread(image_value, 1, 1024, image_file))!=0)
@@ -36,8 +35,22 @@ int main(int argc, char *argv[])
     MD5_Final(hash_value, &mdContext);
 
     int i;
-    for (i=0; i<MD5_DIGEST_LENGTH; i++)
-        sprintf(hash_value1+i, "%02x", hash_value[i]);
-    printf("Hash Value: %s\n", hash_value1);
+    FILE *report = fopen(FILE_LOCATION, "r+");
+    assert(report);
 
+    char cursor;
+    int j = 0;
+    while (j<= pcr_index) {
+        cursor = fgetc(report);
+        if (cursor == 58)
+            j++;
+    }
+
+    for (i=0; i<MD5_DIGEST_LENGTH; i++) {
+        sprintf(&per_value, "%02x", hash_value[i]);
+        fputc(per_value, report);
+        if ((i%2) == 1)
+        fputc(' ', report);
+    }
     return 0;
+}
