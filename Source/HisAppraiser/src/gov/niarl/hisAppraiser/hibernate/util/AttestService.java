9eaf64603e3a0e9be9641006029984f395535430
tianweiz07 <tianweiz@princeton.edu>
2015-07-29 10:22:46 -0400

    Update AttestService.java

--- Source/HisAppraiser/src/gov/niarl/hisAppraiser/hibernate/util/AttestService.java
+++ Source/HisAppraiser/src/gov/niarl/hisAppraiser/hibernate/util/AttestService.java
@@ -42,89 +42,7 @@ import com.sun.jersey.api.client.WebResource;
 
 public class AttestService {
 	
-	public static String GetKernelId(String securityProperty, String vmId){
-		Connection con = null;
-		PreparedStatement st = null;
-		ResultSet rs = null;
-		int row_size;
-		String url = "jdbc:mysql://nebula1:3306/nova";
-		String user = "root";
-		String password = "adminj310a";
-
-		String kernel_id = null;
-
-		try {
-			con = DriverManager.getConnection(url, user, password);
-			st = con.prepareStatement("SELECT * FROM instance_system_metadata");
-			rs = st.executeQuery();
-			rs.last();
-			row_size = rs.getRow();
-			if (row_size > 0) {
-				while (rs.getRow()>0) {
-					if ( (rs.getString("instance_uuid").equals(vmId)) && (rs.getString("key").equals("image_kernel_id"))) {
-						kernel_id = rs.getString("value");
-						break;
-					}
-					else {
-						rs.previous();
-					}
-				}
-			}
-    	   	} catch (SQLException ex) {}
-		finally {
-			try {
-				if (rs != null) 
-					rs.close();
-				if (st != null) 
-					st.close();
-				if (con != null) 
-					con.close();
-			} catch (SQLException ex) {}
-		}
-		return kernel_id;
-	}	
 	
-	public static boolean CheckKernelIntegrity(String kernel_id, String measure_value){
-		Connection con = null;
-		PreparedStatement st = null;
-		ResultSet rs = null;
-		int row_size;
-		String url = "jdbc:mysql://nebula1:3306/oat_db";
-		String user = "root";
-		String password = "adminj310a";
-
-		boolean flag = false;
-
-		try {
-			con = DriverManager.getConnection(url, user, password);
-			st = con.prepareStatement("SELECT * FROM PCR_WHITE_LIST");
-			rs = st.executeQuery();
-			rs.last();
-			row_size = rs.getRow();
-			if (row_size > 0) {
-				while (rs.getRow()>0) {
-					if ( (rs.getString("PCR_NAME").equals(kernel_id)) ) {
-						flag = rs.getString("PCR_DIGEST").equals(measure_value);
-						break;
-					}
-					else {
-						rs.previous();
-					}
-				}
-			}
-    	   	} catch (SQLException ex) {}
-		finally {
-			try {
-				if (rs != null) 
-					rs.close();
-				if (st != null) 
-					st.close();
-				if (con != null) 
-					con.close();
-			} catch (SQLException ex) {}
-		}
-		return flag;
-	}	
 	/**
 	 * validate PCR value of a request. Here is 4 cases, that is timeout, unknown, trusted and untrusted.
 	 * case1 (timeout): attest's time is greater than default timeout of attesting from OpenAttestation.properties. In generally, it is usually set as 60 seconds;
@@ -159,23 +77,6 @@ public class AttestService {
 				}
 			} else {
 				flag = false;
-			}
-			// This is the place for property interpretation. 
-			// You can get meta data from the database based on the requestId in the attestRequest. 
-			// Then set the flag as true (trusted) or false(untrusted).
-			String securityProperty = attestRequest.getSecurityProperty();
-			String vmId = attestRequest.getVmId();
-
-			// Here show an example of VM kernel imtegrity. Other cases can be added here.
-			if (securityProperty.equals("1")) {
-				String kernel_id = GetKernelId(securityProperty, vmId);
-				String measure_value = pcrs.get(1);
-				if (kernel_id != null) {
-					flag = CheckKernelIntegrity(kernel_id, measure_value);
-				}
-				else {
-					flag = false;
-				}
 			}
 
 			if (!flag){
