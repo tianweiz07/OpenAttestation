14b73e71cef465055fdc4646dbb182b8910d9a20
tianweiz07 <tianweiz@princeton.edu>
2015-09-01 02:31:07 -0400

    Update migrate.py

--- perf_attest/migrate.py
+++ perf_attest/migrate.py
@@ -8,7 +8,8 @@
 import os
 import string
 import sys 
-
+import threading
+import time
 import libvirt
 
 VM_UUID = "38f332c5-a1cc-3fb1-1dc4-efcf1ee503bc"
@@ -21,4 +22,14 @@ conn=libvirt.open("qemu+ssh://"+SOURCE_USER_NAME+"@"+SOURCE_HOST_ADDR+"/system")
 conn1=libvirt.open("qemu+ssh://"+DEST_USER_NAME+"@"+DEST_HOST_ADDR+"/system")
 
 dom0 = conn.lookupByUUIDString(VM_UUID)
-dom0.migrate(conn1, 1, None, None, 0)
+
+threads = []
+t1 = threading.Thread(target=dom0.migrate, args=(conn1, 1, None, None, 0))
+t2 = threading.Thread(target=dom0.migrateSetMaxDowntime, args=(1000, 0))
+threads.append(t1)
+threads.append(t2)
+t1.start()
+time.sleep(5)
+t2.start()
+
+
